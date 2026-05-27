f3e1c041546b1f8fac2028f0f95a556ccb30a2d0
maximalekseenko <max.alekseenko@gmail.com>
2026-05-27 16:45:31 +0300

    public gui combiner added

---
+++ include/interface/gui.h
@@ -0,0 +1,12 @@
+// Copyright 2026 maxim (necromax) alekseenko
+
+#ifndef INCLUDE_INTERFACE_GUI_H_
+#define INCLUDE_INTERFACE_GUI_H_
+
+#include "interface/gui/component.h"
+#include "interface/gui/lumen_rules.h"
+
+using GuiComponent = interfaceengine::gui::Component;
+using LumenRules = LumenRules;
+
+#endif  // INCLUDE_INTERFACE_GUI_H_
